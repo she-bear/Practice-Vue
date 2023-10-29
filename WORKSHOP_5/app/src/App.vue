f444e24848449752c10ecf0ce6cdaa799862d7f2
Olga Kutueva <kutueva.olga@gmail.com>
2023-10-29 14:00:48 +0300

    Workshop 5, задание 1

---
+++ WORKSHOP_5/app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- значение isAuth будет принято в самом компоненте -->
+  <AuthComponent :isAuth="false" />
+</template>
+<script>
+
+import AuthComponent from './components/AuthComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AuthComponent,
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ WORKSHOP_5/app/src/components/AuthComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="isAuth">
+        <h2>Authentication Successful</h2>
+    </div>
+    <div v-else>
+        <h2>Registration required!</h2>
+        <form>
+            <label>Name<input type="text"></label>
+            <label>Email<input type="email"></label>
+            <label>Password<input type="password"></label>
+            <button>
+                Submit
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isAuth: Boolean,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
