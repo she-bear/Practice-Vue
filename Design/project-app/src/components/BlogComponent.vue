91094bdbc38d749cf61d095306a842a6b4de266d
Olga Kutueva <kutueva.olga@gmail.com>
2023-10-29 19:17:37 +0300

    FooterComponent

--- Design/project-app/src/components/BlogComponent.vue
+++ Design/project-app/src/components/BlogComponent.vue
@@ -55,40 +55,14 @@
         </div>
     </section>
 
-    <footer class="footer center">
-        <div class="footer__column">
-            <div class="logo">
-                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                    <path
-                        d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-                        fill="#CDA274" />
-                </svg>
-                <a href="index.html" class="logo__title">Interno</a>
-            </div>
-            <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
-            <div class="footer__icons">
-                <img :src="twitterIcon" alt="Twitter icon">
-                <img :src="inIcon" alt="IN icon">
-            </div>
-        </div>
-        <div class="footer__column footer__column_shifted">
-            <p class="footer__title">Pages</p>
-            <a href="" class="footer__text">Home</a>
-            <a href="" class="footer__text">Project</a>
-            <a href="" class="footer__text">Blog</a>
-        </div>
-        <div class="footer__column footer__column_shifted">
-            <p class="footer__title">Contact</p>
-            <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
-            <p class="footer__text">contact@interno.com</p>
-            <p class="footer__text">(123) 456 - 7890</p>
-        </div>
-    </footer>
+    <FooterComponent />
 </template>
 
 <script>
 
 import HeaderComponent from "./HeaderComponent.vue";
+import FooterComponent from "./FooterComponent.vue";
+
 
 import topImage from "@/assets/img/top_blog.png";
 import latestPostImage from "@/assets/img/latest_post_image.png";
@@ -102,12 +76,11 @@ import paginationImage_1 from "@/assets/img/pagination_01.svg";
 import paginationImage_2 from "@/assets/img/pagination_02.svg";
 import paginationImage_3 from "@/assets/img/pagination_03.svg";
 import paginationImage_more from "@/assets/img/pagination_more.svg";
-import twitterIcon from "@/assets/img/twitter_icon.svg";
-import inIcon from "@/assets/img/in_icon.svg";
 
 export default {
     components: {
         HeaderComponent,
+        FooterComponent,
     },
     data() {
         const article = [
@@ -147,8 +120,6 @@ export default {
             article: article,
             topImage,
             latestPostImage,
-            twitterIcon,
-            inIcon,
             paginationImage_1,
             paginationImage_2,
             paginationImage_3,
--- Design/project-app/src/components/MainPageComponent.vue
+++ Design/project-app/src/components/MainPageComponent.vue
@@ -68,40 +68,13 @@
         </div>
     </section>
 
-    <footer class="footer center">
-        <div class="footer__column">
-            <div class="logo">
-                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                    <path
-                        d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-                        fill="#CDA274" />
-                </svg>
-                <a href="index.html" class="logo__title">Interno</a>
-            </div>
-            <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
-            <div class="footer__icons">
-                <img :src="twitterIcon" alt="Twitter icon">
-                <img :src="inIcon" alt="IN icon">
-            </div>
-        </div>
-        <div class="footer__column footer__column_shifted">
-            <p class="footer__title">Pages</p>
-            <a href="" class="footer__text">Home</a>
-            <a href="" class="footer__text">Project</a>
-            <a href="" class="footer__text">Blog</a>
-        </div>
-        <div class="footer__column footer__column_shifted">
-            <p class="footer__title">Contact</p>
-            <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
-            <p class="footer__text">contact@interno.com</p>
-            <p class="footer__text">(123) 456 - 7890</p>
-        </div>
-    </footer>
+    <FooterComponent />
 </template>
 
 <script>
 
 import HeaderComponent from "./HeaderComponent.vue";
+import FooterComponent from "./FooterComponent.vue";
 
 import mainImage from "@/assets/img/main_image.png";
 import projectImage_1 from "@/assets/img/project_photo1.png";
@@ -111,13 +84,11 @@ import projectImage_4 from "@/assets/img/project_photo4.png";
 import articleImage_1 from "@/assets/img/article_photo1.png";
 import articleImage_2 from "@/assets/img/article_photo2.png";
 import articleImage_3 from "@/assets/img/article_photo3.png";
-import twitterIcon from "@/assets/img/twitter_icon.svg";
-import inIcon from "@/assets/img/in_icon.svg";
-
 
 export default {
     components: {
         HeaderComponent,
+        FooterComponent,
     },
     data() {
         const projects = [
@@ -190,8 +161,6 @@ export default {
             projects: projects,
             statistic: statistic,
             article: article,
-            twitterIcon,
-            inIcon,
         }
     },
 
