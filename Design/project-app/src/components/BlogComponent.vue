adeb468661b3f663adae8fae294b7efaade41865
Olga Kutueva <kutueva.olga@gmail.com>
2023-10-29 19:11:21 +0300

    HeaderComponent

--- Design/project-app/src/components/BlogComponent.vue
+++ Design/project-app/src/components/BlogComponent.vue
@@ -1,6 +1,62 @@
 <template>
-    <div>
-        <header class="header center">
+    <HeaderComponent />
+
+    <section class="blog__top">
+        <img :src="topImage" alt="Top blog image">
+        <div class="blog__top__intro">
+            <h2 class="blog__top__title">Articles &amp; News</h2>
+            <p class="blog__top__text">Home / Blog</p>
+        </div>
+    </section>
+
+    <section class="latest__post center">
+        <h2 class="latest__post__title">Latest Post</h2>
+        <div class="latest__post__content">
+            <img :src="latestPostImage" alt="Latest post image">
+            <div class="latest__post__left">
+                <h3 class="latest__post__subtitle">Low Cost Latest Invented Interior Designing Ideas</h3>
+                <p class="latest__post__text">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                    turpisdignissim maximus.posuere in.Contrary to popular belief.</p>
+                <p class="latest__post__text">Lorem Ipsum is not simply random text. It has roots in a piece of
+                    classica.</p>
+                <div class="latest__post__bottom">
+                    <p class="latest__post__data">26 December, 2022 </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
+                        <circle cx="26" cy="26.5" r="26" fill="#F4F0EC" />
+                        <path d="M23.771 33.1855L29.7139 26.4998L23.771 19.8141" stroke="#292F36" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="articles center">
+        <h2 class="articles__title">Articles &amp; News</h2>
+        <div class="articles__content">
+            <article v-for="(article, index) in article" :key="index" class="articles__item">
+                <img :src="article.img" alt="Article photo">
+                <h3 class="articles__item__title">{{ article.title }}</h3>
+                <div class="articles__item__bottom">
+                    <p class="articles__item__data">{{ article.date }} </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
+                        <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
+                        <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </article>
+        </div>
+        <div class="articles__pagination">
+            <img :src="paginationImage_1" alt="pagination icon">
+            <img :src="paginationImage_2" alt="pagination icon">
+            <img :src="paginationImage_3" alt="pagination icon">
+            <img :src="paginationImage_more" alt="pagination icon">
+        </div>
+    </section>
+
+    <footer class="footer center">
+        <div class="footer__column">
             <div class="logo">
                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
                     <path
@@ -9,100 +65,30 @@
                 </svg>
                 <a href="index.html" class="logo__title">Interno</a>
             </div>
-            <nav class="header__right">
-                <a href="#" class="header__right__item">Home</a>
-                <a href="#" class="header__right__item">Project</a>
-                <a href="#" class="header__right__item">Blog</a>
-            </nav>
-        </header>
-
-        <section class="blog__top">
-            <img :src="topImage" alt="Top blog image">
-            <div class="blog__top__intro">
-                <h2 class="blog__top__title">Articles &amp; News</h2>
-                <p class="blog__top__text">Home / Blog</p>
+            <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
+            <div class="footer__icons">
+                <img :src="twitterIcon" alt="Twitter icon">
+                <img :src="inIcon" alt="IN icon">
             </div>
-        </section>
-
-        <section class="latest__post center">
-            <h2 class="latest__post__title">Latest Post</h2>
-            <div class="latest__post__content">
-                <img :src="latestPostImage" alt="Latest post image">
-                <div class="latest__post__left">
-                    <h3 class="latest__post__subtitle">Low Cost Latest Invented Interior Designing Ideas</h3>
-                    <p class="latest__post__text">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
-                        turpisdignissim maximus.posuere in.Contrary to popular belief.</p>
-                    <p class="latest__post__text">Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classica.</p>
-                    <div class="latest__post__bottom">
-                        <p class="latest__post__data">26 December, 2022 </p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
-                            <circle cx="26" cy="26.5" r="26" fill="#F4F0EC" />
-                            <path d="M23.771 33.1855L29.7139 26.4998L23.771 19.8141" stroke="#292F36" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="articles center">
-            <h2 class="articles__title">Articles &amp; News</h2>
-            <div class="articles__content">
-                <article v-for="(article, index) in article" :key="index" class="articles__item">
-                    <img :src="article.img" alt="Article photo">
-                    <h3 class="articles__item__title">{{ article.title }}</h3>
-                    <div class="articles__item__bottom">
-                        <p class="articles__item__data">{{ article.date }} </p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
-                            <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
-                            <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                </article>
-            </div>
-            <div class="articles__pagination">
-                <img :src="paginationImage_1" alt="pagination icon">
-                <img :src="paginationImage_2" alt="pagination icon">
-                <img :src="paginationImage_3" alt="pagination icon">
-                <img :src="paginationImage_more" alt="pagination icon">
-            </div>
-        </section>
-
-        <footer class="footer center">
-            <div class="footer__column">
-                <div class="logo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                        <path
-                            d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-                            fill="#CDA274" />
-                    </svg>
-                    <a href="index.html" class="logo__title">Interno</a>
-                </div>
-                <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
-                <div class="footer__icons">
-                    <img :src="twitterIcon" alt="Twitter icon">
-                    <img :src="inIcon" alt="IN icon">
-                </div>
-            </div>
-            <div class="footer__column footer__column_shifted">
-                <p class="footer__title">Pages</p>
-                <a href="" class="footer__text">Home</a>
-                <a href="" class="footer__text">Project</a>
-                <a href="" class="footer__text">Blog</a>
-            </div>
-            <div class="footer__column footer__column_shifted">
-                <p class="footer__title">Contact</p>
-                <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
-                <p class="footer__text">contact@interno.com</p>
-                <p class="footer__text">(123) 456 - 7890</p>
-            </div>
-        </footer>
-    </div>
+        </div>
+        <div class="footer__column footer__column_shifted">
+            <p class="footer__title">Pages</p>
+            <a href="" class="footer__text">Home</a>
+            <a href="" class="footer__text">Project</a>
+            <a href="" class="footer__text">Blog</a>
+        </div>
+        <div class="footer__column footer__column_shifted">
+            <p class="footer__title">Contact</p>
+            <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
+            <p class="footer__text">contact@interno.com</p>
+            <p class="footer__text">(123) 456 - 7890</p>
+        </div>
+    </footer>
 </template>
 
 <script>
+
+import HeaderComponent from "./HeaderComponent.vue";
 
 import topImage from "@/assets/img/top_blog.png";
 import latestPostImage from "@/assets/img/latest_post_image.png";
@@ -120,6 +106,9 @@ import twitterIcon from "@/assets/img/twitter_icon.svg";
 import inIcon from "@/assets/img/in_icon.svg";
 
 export default {
+    components: {
+        HeaderComponent,
+    },
     data() {
         const article = [
             {
--- Design/project-app/src/components/MainPageComponent.vue
+++ Design/project-app/src/components/MainPageComponent.vue
@@ -1,6 +1,75 @@
 <template>
-    <div>
-        <header class="header center">
+    <HeaderComponent />
+
+    <section class="main center">
+        <img :src="mainImage" alt="Main top image">
+        <div class="main__content">
+            <h2 class="main__content__title">Let Your Home Be Unique</h2>
+            <h3 class="main__content__subtitle">There are many variations of the passages of lorem Ipsum fromavailable,
+                majority.</h3>
+            <button class="main__content__button">Get Started
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
+                    <path d="M1 8.43542L14.7232 8.29857M9.61818 1.91138L16.1412 8.43436L9.48677 15.0887" stroke="#CDA274"
+                        stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
+    </section>
+
+    <section class="projects center">
+        <h2 class="projects__title">Follow Our Projects</h2>
+        <p class="projects__text">It is a long established fact that a reader will be distracted by the of readable
+            content of page lookings at its layouts points.</p>
+        <div class="projects__content">
+            <article v-for="(project, index) in projects" :key="index" class="projects__item">
+                <img :src="project.img" alt="Project image">
+                <div class="projects__item__content">
+                    <div class="projects__item__content__text">
+                        <h3 class="projects__item__content__title">{{ project.title }}</h3>
+                        <p class="projects__item__content__subtitle">{{ project.route }}</p>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+                        <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
+                        <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <section class="statistic center">
+        <div v-for="(stat, index) in statistic" :key="index" class="statistic__item">
+            <div class="statistic__content">
+                <h2 class="statistic__number">{{ stat.number }}</h2>
+                <p class="statistic__text">{{ stat.text }}</p>
+            </div>
+            <div v-if="index < 3" class="statistic__line"></div>
+        </div>
+    </section>
+
+    <section class="articles center">
+        <h2 class="articles__title">Articles & News</h2>
+        <p class="articles__text">It is a long established fact that a reader will be distracted by the of readable
+            content of a page when lookings at its layouts the points of using.</p>
+        <div class="articles__content">
+            <article v-for="(article, index) in article" :key="index" class="articles__item">
+                <img :src="article.img" alt="Article photo">
+                <h3 class="articles__item__title">{{ article.title }}</h3>
+                <div class="articles__item__bottom">
+                    <p class="articles__item__data">{{ article.date }}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
+                        <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
+                        <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <footer class="footer center">
+        <div class="footer__column">
             <div class="logo">
                 <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
                     <path
@@ -9,113 +78,30 @@
                 </svg>
                 <a href="index.html" class="logo__title">Interno</a>
             </div>
-            <nav class="header__right">
-                <a href="#" class="header__right__item">Home</a>
-                <a href="#" class="header__right__item">Project</a>
-                <a href="#" class="header__right__item">Blog</a>
-            </nav>
-        </header>
-
-        <section class="main center">
-            <img :src="mainImage" alt="Main top image">
-            <div class="main__content">
-                <h2 class="main__content__title">Let Your Home Be Unique</h2>
-                <h3 class="main__content__subtitle">There are many variations of the passages of lorem Ipsum fromavailable,
-                    majority.</h3>
-                <button class="main__content__button">Get Started
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
-                        <path d="M1 8.43542L14.7232 8.29857M9.61818 1.91138L16.1412 8.43436L9.48677 15.0887"
-                            stroke="#CDA274" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
-                    </svg>
-                </button>
+            <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
+            <div class="footer__icons">
+                <img :src="twitterIcon" alt="Twitter icon">
+                <img :src="inIcon" alt="IN icon">
             </div>
-        </section>
-
-        <section class="projects center">
-            <h2 class="projects__title">Follow Our Projects</h2>
-            <p class="projects__text">It is a long established fact that a reader will be distracted by the of readable
-                content of page lookings at its layouts points.</p>
-            <div class="projects__content">
-                <article v-for="(project, index) in projects" :key="index" class="projects__item">
-                    <img :src="project.img" alt="Project image">
-                    <div class="projects__item__content">
-                        <div class="projects__item__content__text">
-                            <h3 class="projects__item__content__title">{{ project.title }}</h3>
-                            <p class="projects__item__content__subtitle">{{ project.route }}</p>
-                        </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
-                            <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
-                            <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                </article>
-            </div>
-        </section>
-
-        <section class="statistic center">
-            <div v-for="(stat, index) in statistic" :key="index" class="statistic__item">
-                <div class="statistic__content">
-                    <h2 class="statistic__number">{{ stat.number }}</h2>
-                    <p class="statistic__text">{{ stat.text }}</p>
-                </div>
-                <div v-if="index < 3" class="statistic__line"></div>
-            </div>
-        </section>
-
-        <section class="articles center">
-            <h2 class="articles__title">Articles & News</h2>
-            <p class="articles__text">It is a long established fact that a reader will be distracted by the of readable
-                content of a page when lookings at its layouts the points of using.</p>
-            <div class="articles__content">
-                <article v-for="(article, index) in article" :key="index" class="articles__item">
-                    <img :src="article.img" alt="Article photo">
-                    <h3 class="articles__item__title">{{ article.title }}</h3>
-                    <div class="articles__item__bottom">
-                        <p class="articles__item__data">{{ article.date }}</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none">
-                            <circle cx="26" cy="26.267" r="26" fill="#F4F0EC" />
-                            <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                </article>
-            </div>
-        </section>
-
-        <footer class="footer center">
-            <div class="footer__column">
-                <div class="logo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-                        <path
-                            d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-                            fill="#CDA274" />
-                    </svg>
-                    <a href="index.html" class="logo__title">Interno</a>
-                </div>
-                <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
-                <div class="footer__icons">
-                    <img :src="twitterIcon" alt="Twitter icon">
-                    <img :src="inIcon" alt="IN icon">
-                </div>
-            </div>
-            <div class="footer__column footer__column_shifted">
-                <p class="footer__title">Pages</p>
-                <a href="" class="footer__text">Home</a>
-                <a href="" class="footer__text">Project</a>
-                <a href="" class="footer__text">Blog</a>
-            </div>
-            <div class="footer__column footer__column_shifted">
-                <p class="footer__title">Contact</p>
-                <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
-                <p class="footer__text">contact@interno.com</p>
-                <p class="footer__text">(123) 456 - 7890</p>
-            </div>
-        </footer>
-    </div>
+        </div>
+        <div class="footer__column footer__column_shifted">
+            <p class="footer__title">Pages</p>
+            <a href="" class="footer__text">Home</a>
+            <a href="" class="footer__text">Project</a>
+            <a href="" class="footer__text">Blog</a>
+        </div>
+        <div class="footer__column footer__column_shifted">
+            <p class="footer__title">Contact</p>
+            <p class="footer__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
+            <p class="footer__text">contact@interno.com</p>
+            <p class="footer__text">(123) 456 - 7890</p>
+        </div>
+    </footer>
 </template>
 
 <script>
+
+import HeaderComponent from "./HeaderComponent.vue";
 
 import mainImage from "@/assets/img/main_image.png";
 import projectImage_1 from "@/assets/img/project_photo1.png";
@@ -130,6 +116,9 @@ import inIcon from "@/assets/img/in_icon.svg";
 
 
 export default {
+    components: {
+        HeaderComponent,
+    },
     data() {
         const projects = [
             {
