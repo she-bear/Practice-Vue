3552f3ef223064ff34f5802243bef448dd3dfb9b
Olga Kutueva <kutueva.olga@gmail.com>
2023-10-31 14:03:21 +0300

    Workshop 6. Задание 1

---
+++ WORKSHOP_6/app/src/components/BookComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>{{ book.title }}</h2>
+        <p>{{ book.author }}</p>
+        <button @click="toggleBtn">{{ showBtn ? "Hide details" : "Show details" }}</button>
+        <div v-if="showBtn">
+            <p>{{ book.year }}</p>
+            <p>{{ book.pages }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // имя компонента (будет отображаться во Vue Devtools)
+    name: "Book",
+    data() {
+        return {
+            showBtn: false,
+        };
+    },
+    methods: {
+        toggleBtn() {
+            this.showBtn = !this.showBtn;
+        }
+    },
+    props: {
+        book: Object,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ WORKSHOP_6/app/src/components/BookListComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <BookComponent v-for="book in booksArray" :key="book.id" :book="book" />
+</template>
+
+<script>
+import BookComponent from './BookComponent.vue';
+
+export default {
+    data() {
+        return {
+            booksArray: [
+                {
+                    id: 0,
+                    title: 'Book1',
+                    author: 'Author 1',
+                    year: 2001,
+                    pages: 125,
+                },
+                {
+                    id: 1,
+                    title: 'Book2',
+                    author: 'Author 2',
+                    year: 2005,
+                    pages: 1345,
+                },
+                {
+                    id: 2,
+                    title: 'Book3',
+                    author: 'Author 3',
+                    year: 1975,
+                    pages: 340,
+                }
+            ]
+        };
+    },
+    components: { BookComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
