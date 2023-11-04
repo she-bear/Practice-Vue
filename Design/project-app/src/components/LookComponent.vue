510450afaaffd80f8387b3658170f86ee7f7dad2
Olga Kutueva <kutueva.olga@gmail.com>
2023-11-04 22:31:52 +0300

    Look Component, Slider

---
+++ Design/project-app/src/components/LookComponent.vue
@@ -0,0 +1,106 @@
+<template>
+    <HeaderComponent />
+
+    <section class="blog__top">
+        <img :src="topImage" alt="Top look image">
+    </section>
+    <section class="look center">
+        <h2 class="projects__title">{{ pageData.title }}</h2>
+        <p class="projects__text projects__text-left">{{ pageData.paragraph }}</p>
+    </section>
+
+    <swiper :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+    }" :zoom="true" :navigation="true" :pagination="{
+    clickable: true,
+}" :modules="modules" class="mySwiper">
+
+        <swiper-slide>
+            <div class="swiper-zoom-container">
+                <img :src="swiperImage" alt="image">
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="swiper-zoom-container">
+                <img :src="swiperImage" alt="image">
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="swiper-zoom-container">
+                <img :src="swiperImage" alt="image">
+            </div>
+        </swiper-slide>
+    </swiper>
+
+    <FooterComponent />
+</template>
+
+<script>
+import FooterComponent from './FooterComponent.vue';
+import HeaderComponent from './HeaderComponent.vue';
+import { mapState } from 'vuex';
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/zoom';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Zoom, Navigation, Pagination } from 'swiper/modules';
+
+import topImage from "@/assets/img/top_look.png";
+import swiperImage from "@/assets/img/look_image.png";
+
+export default {
+    components: {
+        HeaderComponent,
+        FooterComponent,
+        Swiper,
+        SwiperSlide
+    },
+    setup() {
+        return {
+            modules: [Zoom, Navigation, Pagination],
+        };
+    },
+    computed: {
+        ...mapState(['pageData']),
+    },
+
+    data() {
+        return {
+            topImage,
+            swiperImage,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.look {
+    padding-top: 150px;
+    padding-bottom: 97px;
+    display: flex;
+    flex-direction: column;
+}
+
+.projects__text-left {
+    text-align: left;
+}
+
+.swiper {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 100px;
+}
+
+.swiper-slide {
+    overflow: hidden;
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+    color: black !important;
+}
+</style>
+
--- Design/project-app/src/store/index.js
+++ Design/project-app/src/store/index.js
@@ -1,5 +1,13 @@
 import { createStore } from 'vuex';
 
 export default createStore({
+    state: {
+        // по Заданию 7 передаем только эти данные
+        pageData: {
+            title: 'Minimal Look Bedrooms',
+            paragraph: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo. 
 
+            In nec sem suscipit, convallis leo vitae, lacinia nibh. Cras amet tellus lectus. Vivamus ipsum nunc, mattis quis nibh id, pellentesque arcu. Donec a pellentesque Cras erat enim, gravida non ante vitae,elequis convallis elit, in viverra felis. Donec ultrices tellus vitae iaculisvd porta. Proin tincidunt ligula id purus porttitor.`,
+        },
+    },
 })
