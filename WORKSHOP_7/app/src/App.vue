a7a8070bf8258dee4d4d661a3e40a83ef4bc46c9
Olga Kutueva <kutueva.olga@gmail.com>
2023-11-04 14:03:17 +0300

    Workshop 7. Задание 2

--- WORKSHOP_7/app/src/App.vue
+++ WORKSHOP_7/app/src/App.vue
@@ -3,17 +3,36 @@
   <h3>Count: {{ count }}</h3>
   <button @click="INCREMENT">+++</button>
   <button @click="DECREMENT">---</button>
+
+  <hr>
+
+  <div>
+    <h2>Корзина</h2>
+    <ul>
+      <li v-for="(item, index) in cartItems" :key="index">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+
+    <p>Товаров в корзине: {{ totalItems }}</p>
+    <p>Общая стоимость: {{ totalPrice }}</p>
+
+    <button @click="addToCart({ id: 1, name: 'Товар', price: 10 })">
+      Добавить в корзину</button>
+  </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 
 export default {
   computed: {
-    ...mapState(['count'])
+    ...mapState(['count']),
+    ...mapGetters(['cartItems', 'totalItems', 'totalPrice']),
   },
   methods: {
-    ...mapMutations(['INCREMENT', 'DECREMENT'])
+    ...mapMutations(['INCREMENT', 'DECREMENT']),
+    ...mapActions(['addToCart']),
   }
 }
 </script>
--- WORKSHOP_7/app/src/store/index.js
+++ WORKSHOP_7/app/src/store/index.js
@@ -3,8 +3,20 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     count: 0,
+    cart: [],
   },
   getters: {
+    cartItems(state) {
+      return state.cart;
+    },
+
+    totalItems(state) {
+      return state.cart.length;
+    },
+
+    totalPrice(state) {
+      return state.cart.reduce((total, item) => total + item.price, 0);
+    }
   },
   mutations: {
     INCREMENT(state) {
@@ -17,8 +29,15 @@ export default createStore({
     DECREMENT(state) {
       state.count--;
     },
+    ADD_ITEM_TO_CART(state, item) {
+      state.cart.push(item);
+    }
   },
   actions: {
+    addToCart(context, item) {
+      // метод commit вызовет мутацию и передаст в неё данные из компонента
+      context.commit('ADD_ITEM_TO_CART', item);
+    }
   },
   modules: {
   }
